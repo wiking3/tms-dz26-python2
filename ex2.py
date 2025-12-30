15a6ae68b0e3a453e9a4c066041bbc3bd3686299
Egor <70162439+wiking3@users.noreply.github.com>
2025-12-30 21:01:21 +0300

    Add character counting functionality in ex2.py
    работы со строками

---
+++ ex2.py
@@ -0,0 +1,21 @@
+import string                            #нужен для работы со стороками
+stroka = input ("Введите строку:  ")
+count_low=0
+count_high=0
+count_number=0
+count_punkt=0     
+punctuation_chars = string.punctuation    #содержит массив из символов пунктуации
+for char in stroka:
+    if char.isupper(): 
+            count_high += 1
+    else: 
+         if char.islower():
+            count_low += 1
+         else:
+             if char.isdigit():
+                   count_number += 1
+             else:
+                   if char in punctuation_chars:
+                         count_punkt += 1                  
+
+print(f"low: {count_low} high: {count_high} number: {count_number} punkt: {count_punkt}")
