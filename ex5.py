31a8bb72427c44f29a936e2d096acc7e4cc2ad95
Egor <70162439+wiking3@users.noreply.github.com>
2026-01-01 16:15:35 +0300

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,19 @@
+#скрипт получает строку (элементы,разделенные пробелом) - преобразует в кортеж и проверяет на уникальность все элементы кортежа
+
+def are_elements_unique_loop(my_tuple):     # функция проверяет, все ли элементы в кортеже уникальны, используя цикл
+  seen = []
+  for element in my_tuple:
+    if element in seen:
+      return False                          # Найден дубликат
+    seen.append(element)
+  return True                               # Дубликатов не найдено
+
+input_string = input("Введите символы, разделенные пробелом: ")
+list_of_chars = input_string.split()
+char_tuple = tuple(list_of_chars)          #Преобразуем список в кортеж
+
+
+if are_elements_unique_loop(char_tuple)==False :          #вызываем функцию на "уникальность кортежа" и проверяем ее результат
+   print ("Кортеж не уникален - есть повторяющиеся символы")
+else : 
+   print ("Все символы в кортеже  уникалены")
